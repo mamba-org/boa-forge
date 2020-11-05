ae5e2fdc207e6692bbdadf8f2127fd1d78f32b0d
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-11-05 18:45:55 +0100

    add reproc

---
+++ reproc/build_cpp.sh
@@ -0,0 +1,15 @@
+mkdir build-cpp; cd build-cpp
+
+if [[ "${FEATURE_STATIC}" == "1" ]]; then
+    BUILD_TYPE="-DBUILD_SHARED_LIBS=OFF"
+else
+    BUILD_TYPE="-DBUILD_SHARED_LIBS=ON"
+fi
+
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DREPROC++=ON \
+      ${BUILD_TYPE}
+
+make install -j${CPU_COUNT}
