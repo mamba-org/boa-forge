54c8ff29cff9e85de9c57feafdac16dafe7eaef0
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-03-02 19:04:51 +0100

    add nightly recipe, modify how we build nightly

--- get_latest_conda_package.py
+++ get_latest_conda_package.py
@@ -1,17 +1,33 @@
 import glob
 import sys
+import tarfile
 
-folder = sys.argv[1]
-mms = glob.glob(f'{folder}/micromamba*')
+archs = ["linux-64", "osx-64", "win-64"]
+folder = "~/micromamba_pkgs/{arch}/"
 
-versions = []
-for m in mms:
-    m = m[:-len('.tar.bz2')]  # remove tar.bz2 ending
-    name, version, build = m.rsplit('-', 2)
-    build_num = int(build.rsplit('_', 1)[-1])
-    versions.append((name, version, build_num, build))
+def get_version_file(folder):
+	mms = glob.glob(f'{folder}/micromamba*')
 
-def assemble_file(v):
-    return f"{v[0]}-{v[1]}-{v[3]}.tar.bz2"
+	versions = []
+	for m in mms:
+	    m = m[:-len('.tar.bz2')]  # remove tar.bz2 ending
+	    name, version, build = m.rsplit('-', 2)
+	    build_num = int(build.rsplit('_', 1)[-1])
+	    versions.append((name, version, build_num, build))
 
-print(assemble_file(max(versions)))
+	def assemble_file(v):
+	    return f"{v[0]}-{v[1]}-{v[3]}.tar.bz2"
+
+	print(assemble_file(max(versions)))
+
+	return assemble_file(max(versions)), v[1]
+
+for arch in archs:
+	version, pkg_file = get_version_file(folder.format(arch))
+	print(f"::set-output name={arch.replace('-', '_')}_pkg::{pkg_file}")
+	if arch == "linux-64":
+		print(f"::set-output name=micromamba_version::{version}")
+
+	os.makedirs(f"/tmp/micromamba-{arch}")
+	with tarfile.open(pkg_file, "r:bz2") as f:
+		f.extractall(f"/tmp/micromamba-{arch}/")
