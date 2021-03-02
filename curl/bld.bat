711732bc8b9b49f8d2db332d950bc715378a43d8
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-03-02 11:01:48 +0100

    test micromamba

--- curl/bld.bat
+++ curl/bld.bat
@@ -14,24 +14,3 @@ cmake -GNinja ^
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 ninja install --verbose
-
-REM This is implicitly using WinSSL.  See Makefile.vc for more info.
-rem nmake /f Makefile.vc MODE=dll VC=%VS_MAJOR:"=% WITH_DEVEL=%LIBRARY_PREFIX% ^
-rem          WITH_ZLIB=dll WITH_SSH2=dll DEBUG=no ENABLE_IDN=no ENABLE_SSPI=yes ^
-rem          MACHINE=%ARCH_STRING%
-rem if errorlevel 1 exit 1
-
-REM WITH_SSH2=static
-REM This is implicitly using WinSSL.  See Makefile.vc for more info.
-rem nmake /f Makefile.vc mode=static VC=%VS_MAJOR:"=% WITH_DEVEL=%LIBRARY_PREFIX% ^
-rem          WITH_ZLIB=static DEBUG=no ENABLE_IDN=no ENABLE_SSPI=yes ^
-rem          MACHINE=%ARCH_STRING%
-rem if errorlevel 1 exit 1
-
-rem REM install static library
-rem robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-static-zlib-static-ipv6-sspi-schannel\ %LIBRARY_PREFIX% *.* /E
-rem if %ERRORLEVEL% GTR 3 exit 1
-
-rem REM install everything else
-rem robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ssh2-dll-ipv6-sspi-winssl\ %LIBRARY_PREFIX% *.* /E
-rem if %ERRORLEVEL% GTR 3 exit 1
--- libarchive/bld.bat
+++ libarchive/bld.bat
@@ -71,19 +71,3 @@ ninja -j%CPU_COUNT% -v
 if errorlevel 1 exit /b 1
 ninja install
 if errorlevel 1 exit /b 1
-
-:: Test.
-:: Failures:
-:: The following tests FAILED:
-::         365 - libarchive_test_read_truncated_filter_bzip2 (Timeout) => runs msys2's bzip2.exe
-::         372 - libarchive_test_sparse_basic (Failed)
-::         373 - libarchive_test_fully_sparse_files (Failed)
-::         386 - libarchive_test_warn_missing_hardlink_target (Failed)
-:: ctest -C Release
-:: if errorlevel 1 exit 1
-
-:: Test extracting a 7z. This failed due to not using the multi-threaded DLL runtime, fixed by 0009-CMake-Force-Multi-threaded-DLL-runtime.patch
-rem powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://download.qt.io/development_releases/prebuilt/llvmpipe/windows/opengl32sw-64-mesa_12_0_rc2.7z', 'opengl32sw-64-mesa_12_0_rc2.7z') }"
-rem if errorlevel 1 exit 1
-rem %LIBRARY_BIN%\bsdtar -xf opengl32sw-64-mesa_12_0_rc2.7z
-rem if errorlevel 1 exit 1
