53a2836276918dee5b7a868e9c7d97cac5460f9f
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-11-19 15:44:49 +0100

    use python to_nightly script

---
+++ micromamba_recipe/to_nightly.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+import yaml
+
+if __name__ == '__main__':
+	with open('micromamba_recipe/recipe.yaml') as recipe_in:
+		y = yaml.safe_load(recipe_in)
+
+	y["source"][0] = {
+		'git_url': 'https://github.com/mamba-org/mamba',
+		'git_rev': 'master',
+		'git_depth': 1
+	}
+
+	del y["context"]
+	y["package"] = {
+		'name': 'micromamba-nightly',
+		'version': '{{ environ.get("NIGHTLY_VERSION") }}'
+	}
+
+	with open('micromamba_recipe/recipe.yaml', 'w') as recipe_out:
+		recipe_out.write(yaml.dump(y))
