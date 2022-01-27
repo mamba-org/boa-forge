0e27a05f18eef1af3afa1ab33d46639a6151b19b
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-01-27 11:44:06 +0100

    try reproc

--- bzip2/bld.bat
+++ bzip2/bld.bat
@@ -13,26 +13,3 @@ cmake -G "Ninja" ^
       ..
 
 ninja install --verbose
-
-rem REM Build step
-rem nmake -f makefile.msc
-rem jom -f makefile.msc lib bzip2
-rem if errorlevel 1 exit 1
-
-rem REM fake condition
-rem rem IF ("%FEATURE_STATIC%" != "") (
-rem 	REM Install step
-rem 	copy libbz2.lib %LIBRARY_LIB%\libbz2_static.lib || exit 1
-rem 	REM Some packages expect 'bzip2.lib', so make another copy
-rem 	copy libbz2.lib %LIBRARY_LIB%\bzip2_static.lib || exit 1
-rem 	copy bzlib.h %LIBRARY_INC% || exit 1
-rem ) ELSE (
-rem 	cl /O2 /Ibzlib.h /Ibzlib_private.h /D_USRDLL /D_WINDLL blocksort.c bzlib.c compress.c crctable.c decompress.c huffman.c randtable.c /LD /Felibbz2.dll /link /DEF:libbz2.def
-rem 	if errorlevel 1 exit 1
-
-rem 	copy libbz2.lib %LIBRARY_LIB% || exit 1
-rem 	REM Some packages expect 'bzip2.lib', so make copies
-rem 	copy libbz2.lib %LIBRARY_LIB%\bzip2.lib || exit 1
-rem 	copy libbz2.dll %LIBRARY_BIN% || exit 1
-rem 	copy libbz2.dll %LIBRARY_BIN%\bzip2.dll || exit 1
-rem )
