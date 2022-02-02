ab2904b01d86d139057b0f36f1bef740c6dc1810
michaelkora <michael.koras0@gmail.com>
2022-02-02 16:17:04 +0100

    create a oras push

---
+++ oras.py
@@ -0,0 +1,31 @@
+import logging
+import subprocess
+from pathlib import Path
+
+def getName_andTag(pkg):
+    name, version, hash = pkg.rsplit('-', 2)
+    
+    tag = version + "-" + hash
+    tag_resized = tag.rpartition('.tar')[0]
+    tag_resized = tag_resized.replace("_", "-")
+    
+    return name, tag_resized
+
+
+
+class Oras:
+    def __init__(self, github_owner, origin):
+        self.owner = github_owner
+        self.conda_prefix = origin
+        
+    def push(self, target, data ):
+        pkg = str(data).rsplit('/', 1)[-1]
+        pkg_name, tag = getName_andTag(pkg)
+
+        # upload the tar_bz2 file to the right url
+        push_bz2 = f"oras push ghcr.io/{self.owner}/samples/{target}/{pkg_name}:{tag} {data}:application/octet-stream"
+        upload_url = f"ghcr.io/{self.owner}/samples/{target}/{pkg_name}:{tag}"
+
+        logging.warning(f"Uploading <<{pkg_name}>> (from dir: << {self.conda_prefix} >> to link: <<{upload_url}>>")
+        subprocess.run(push_bz2, shell=True)
+        logging.warning(f"Package <<{pkg_name}>> uploaded to: <<{upload_url}>>")
---
+++ upload_packages.py
@@ -0,0 +1,22 @@
+import sys
+from pathlib import Path
+from oras import Oras
+#initializations 
+owner = sys.argv[1]
+target_platform = str (sys.arg[2])
+conda_prefix = sys.arg[3]
+
+oras = Oras (owner, conda_prefix)
+
+directory = "conda-bld"
+location = Path(conda_prefix) / directory / target_platform
+
+
+for data in location.iterdir():
+    strFile = str(data)
+    if strFile.endswith('tar.bz2'):
+        oras.push(target_platform, data)
+
+
+
+
