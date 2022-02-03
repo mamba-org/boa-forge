d6c41c618b5d98608bfcaf7a68669ee57dd19c6f
michaelkora <michael.koras0@gmail.com>
2022-02-03 11:49:41 +0100

    create a oras push 00.4

--- oras.py
+++ oras.py
@@ -25,23 +25,18 @@ class Oras:
         pkg = str(data).rsplit('/', 1)[-1]
         length = len(strData) - len(pkg)
         path = strData[:length]
-
         pkg_name, tag = getName_andTag(pkg)
 
         origin = "./" + pkg
 
-        logging.warning(f"The extracted pkg <<{pkg}>>.")
-        logging.warning(f"The path is <<{path}>>.")
-
         # upload the tar_bz2 file to the right url
         push_bz2 = f"oras push ghcr.io/{self.owner}/samples/{target}/{pkg_name}:{tag} {origin}:application/octet-stream"
         upload_url = f"ghcr.io/{self.owner}/samples/{target}/{pkg_name}:{tag}"
+        logging.warning(f"Cmd <<{push_bz2}>>")
         chdir(path)
         cur = Path.cwd()
         
 
         logging.warning(f"Uploading <<{pkg}>>. path <<{origin} (from dir: << {self.conda_prefix} >> to link: <<{upload_url}>>")
-        logging.warning(f"current dir is <<{cur}>>")
-        subprocess.run("ls -al", shell=True)
         subprocess.run(push_bz2, shell=True)
         logging.warning(f"Package <<{pkg_name}>> uploaded to: <<{upload_url}>>")
